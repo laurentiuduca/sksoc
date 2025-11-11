de1f6f3c61ca9f708a23669ce7e6f82425d18863
Laurentiu-Cristian Duca <laurentiu.duca@gmail.com>
2025-11-11 16:05:56 +0200

    hdl

--- hdl/debug/dm/hazard3_dm.f
+++
@@ -1,1 +0,0 @@
-file hazard3_dm.v
--- hdl/debug/dtm/hazard3_ecp5_jtag_dtm.f
+++
@@ -1,6 +0,0 @@
-file hazard3_ecp5_jtag_dtm.v
-file hazard3_jtag_dtm_core.v
-
-file ../cdc/hazard3_apb_async_bridge.v
-file ../cdc/hazard3_reset_sync.v
-file ../cdc/hazard3_sync_1bit.v
