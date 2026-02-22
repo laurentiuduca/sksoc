1d810c5a1c474f70614a10624d7f589521a4723f
Laurentiu-Cristian Duca <laurentiu.duca@gmail.com>
2026-02-22 17:43:20 +0200

    1

--- run-nuttx.sh
+++
@@ -1,9 +0,0 @@
-set -x
-set -e
-cd /home/laur/lucru/cn/riscv/rvsoc-site-japan/initmem_gen2
-./run-nuttx.sh
-cd -
-cp /home/laur/lucru/cn/riscv/rvsoc-site-japan/initmem_gen2/init_kernel.txt init_kernel.txt
-make veriwt
-#./simv
-
